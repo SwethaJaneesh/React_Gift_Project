8b3e410f3e3857eed243f6e1dff8b61ab45b6e6a
SwethaJaneesh <125295441+SwethaJaneesh@users.noreply.github.com>
2024-01-04 16:04:40 +0530

    init

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -22,91 +22,89 @@ const Navbar = () => {
    <div class="area"></div><nav class="main-menu">
               <ul>
                   <li>
-                      <a href="https://jbfarrow.com">
+                      <span class="https://jbfarrow.com">
                           <i class="fa fa-home fa-2x"></i>
                           <span class="nav-text">
                              Community Dashboard
                           </span>
-                      </a>
+                      </span>
                     
                   </li>
                   <li class="has-subnav">
-                      <a href="#">
+                      <span class="#">
                           <i class="fa fa-globe fa-2x"></i>
                           <span class="nav-text">
                               Global Surveyors
                           </span>
-                      </a>
-                      
+                      </span>
                   </li>
                   <li class="has-subnav">
-                      <a href="#">
+                      <span class="#">
                          <i class="fa fa-comments fa-2x"></i>
                           <span class="nav-text">
                               Group Hub Forums
                           </span>
-                      </a>
-                      
+                      </span>
                   </li>
                   <li class="has-subnav">
-                      <a href="#">
+                      <span class="#">
                          <i class="fa fa-camera-retro fa-2x"></i>
                           <span class="nav-text">
                               Survey Photos
                           </span>
-                      </a>
+                      </span>
                      
                   </li>
                   <li>
-                      <a href="#">
+                      <span class="#">
                           <i class="fa fa-film fa-2x"></i>
                           <span class="nav-text">
                               Surveying Tutorials
                           </span>
-                      </a>
+                      </span>
                   </li>
                   <li>
-                      <a href="#">
+                      <span class="#">
                           <i class="fa fa-book fa-2x"></i>
                           <span class="nav-text">
                              Surveying Jobs
                           </span>
-                      </a>
+                      </span>
                   </li>
                   <li>
-                     <a href="#">
+                     <span class="#">
                          <i class="fa fa-cogs fa-2x"></i>
                           <span class="nav-text">
                               Tools & Resources
                           </span>
-                      </a>
+                      </span>
                   </li>
                   <li>
-                     <a href="#">
+                     <span class="#">
                           <i class="fa fa-map-marker fa-2x"></i>
                           <span class="nav-text">
                               Member Map
                           </span>
-                      </a>
+                      </span>
                   </li>
                   <li>
-                      <a href="#">
+                      <span class="#">
                          <i class="fa fa-info fa-2x"></i>
                           <span class="nav-text">
                               Documentation
                           </span>
-                      </a>
+                      </span>
                   </li>
               </ul>
   
               <ul class="logout">
                   <li>
-                     <a href="#">
+                     <span class="#">
                            <i class="fa fa-power-off fa-2x"></i>
                           <span class="nav-text">
                               Logout
                           </span>
-                      </a>
+                      </span>
                   </li>  
               </ul>
           </nav>
